88aeeeca6171c195bd604627bc24d21e3d1cb8ee
Haider Kamran <haiderkamrann@gmail.com>
2023-10-26 02:10:43 +0500

    Web Server Using Express JS

--- 2.WebServerExpress.js
+++ 2.WebServerExpress.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Exp!')
+  res.send('Hello Express Server!')
 })
 
 app.listen(3000);
+
