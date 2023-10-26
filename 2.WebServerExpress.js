c276fed1dd13e75ac73b04ab143767a8ecb1f2cb
Haider Kamran <haiderkamrann@gmail.com>
2023-10-26 17:44:36 +0500

    Express Server

--- 2.WebServerExpress.js
+++ 2.WebServerExpress.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 
+const hostname = '127.0.0.1'
+const port = 3000
+
 app.get('/', function (req, res) {
   res.send('Hello Express Server!')
 })
 
-app.listen(3000);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
