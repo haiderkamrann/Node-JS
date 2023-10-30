a610325fb0baa39d3dd16d118ccfbe6abc480572
Haider Kamran <haiderkamrann@gmail.com>
2023-10-30 23:49:13 +0500

    Added Template Engine - Ejs

---
+++ 4. Template Engine - EJS/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.render('index', )
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
