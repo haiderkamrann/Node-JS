c175b45ad8b083ad87f323e8626424cb7c0a48f9
Haider Kamran <haiderkamrann@gmail.com>
2023-11-01 17:45:04 +0500

    Post Request Using Express

---
+++ 5. PostReq - Express/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Handle the form submission
+app.post("/process-user", (req, res) => {
+  res.send(`Name: ${req.body.name} <br> Email: ${req.body.email}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
